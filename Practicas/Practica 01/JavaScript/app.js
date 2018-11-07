51280c2864c04c0afbfcbdbbd061574cfe97fc70
Eduardo Romero Palencia <eduarrom@ucm.es>
2018-11-07 21:32:48 +0100

    Adaptado login

--- Practicas/Practica 01/JavaScript/app.js
+++ Practicas/Practica 01/JavaScript/app.js
@@ -1,39 +1,66 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var http = require('http');
+var dao = require('dao');
 
 var app = express();
 
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
-app.use(express.static('../'));
+app.use(express.static('.'));
 
 app.listen(3000, function () {
   console.log('Practica 1 en el puerto 3000');
 });
 
-var login = require('login');
+let usuario = {
+	email: null,
+	nombre: null,
+	sexo: null,
+	fechaNacimiento: null,
+	fotoPerfil: null
+}
 
-app.post('/login', function (req, res) {
-	if (login.parsePass(req.body.pass)){
-		login.getUsers(req.body.email, req.body.pass, function(err, rows){
+app.get('/usuarioRegistrado/' + /.*/, function(request, response){
+	if (usuario.email == null){
+		response.redirect("/login.html");
+	}
+})
+
+app.get('/', function(request, response){
+	if (usuario.email == null){
+		response.redirect("/login.html");
+	} else {
+		response.redirect("/usuarioRegistrado/friends.html");
+	}
+})
+
+app.post('/login', function (request, response) {
+	if (dao.parsePass(request.body.pass)){
+		dao.getUsers(request.body.email, request.body.pass, function(err, rows){
 			if (err) {
-				throw err;
+				response.write(err.message);
+			} else {			
+				if (rows.length == 0){
+					response.redirect("/login.html");
+				} else if (rows[0].pass != request.body.pass) {
+					response.redirect("/login.html");
+				} else {
+					response.redirect("/usuarioRegistrado/friends.html");
+					usuario.email = rows[0].email;
+					usuario.nombre = rows[0].nombre;
+					usuario.sexo = rows[0].sexo;
+					usuario.fechaNacimiento = rows[0].fechaNacimiento;
+					usuario.imagenPerfil = rows[0]
+				}
 			}
-			
-			if (rows.length == 0){
-				res.redirect("/login.html")
-			} else if (rows[0].password != req.body.pass) {
-				res.redirect("/login.html")
-			} else {
-				res.redirect("/friends.html")
-			}
-			return rows;
 		})
 	} else {
-
-		res.redirect("/login.html")
+		response.redirect("/login.html");
 	}
-
 });
+
+app.get('./usuarioRegistrado/my_profile.html', function(request, response){
+	response.
+	request.document.getElementById("nombreUsuario").write(usuario.nombre);
+})
