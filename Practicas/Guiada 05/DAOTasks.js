c6f571fa8d07148a98497f1382223ae0276dea73
Eduardo Romero Palencia <eduarrom@ucm.es>
2018-11-22 16:01:34 +0100

    Guiada 05

---
+++ Practicas/Guiada 05/DAOTasks.js
@@ -0,0 +1,180 @@
+'use strict'
+
+class DAOTasks {
+    constructor(pool) {
+        this.pool = pool;
+    }
+
+    getAllTasks(email, callback) {
+        this.pool.getConnection(function(err, connection){
+            if(err){
+                callback(new Error("Error de conexion a la base de datos"), null);
+            }else{
+                connection.query(
+                    "select * from task left join tag on tag.taskId = task.id where task.user = ? order by task.id",
+                    [email],
+                    function(err, rows){
+                        connection.release();
+                        if(err){
+                            callback(new Error("Error de acceso a la base de datos"),null)
+                        } else {
+                            let tareas = [];
+                            let tareasInsertadas = 0;
+                            rows.forEach(function(e){
+                                if (tareas[tareasInsertadas - 1] != null && tareas[tareasInsertadas - 1].id == e.id){
+                                    if (e.tag != null){
+                                        tareas[tareasInsertadas - 1].tags.push(e.tag);
+                                    }
+                                } else {
+                                    tareas.push({
+                                        id: e.id,
+                                        text: e.text,
+                                        done: e.done,
+                                        tags: []
+                                    });
+                                    if (e.tag != null){
+                                        tareas[tareasInsertadas].tags.push(e.tag);
+                                    }
+                                    tareasInsertadas++;
+                                }
+                            });
+                            callback(null, tareas);
+                        }
+                    }
+                )
+            }
+        })
+    }
+
+    insertTask(email, task, callback) {
+        this.pool.getConnection(function(err, connection){
+            if(err){
+                callback(new Error("Error de conexion a la base de datos"), null);
+            }else{         
+                let tarea = parsearTarea(task);
+                if(tarea.text != ""){
+                    connection.query(
+                        "insert into task (user,text,done) values (?,?,?)",
+                        [email,tarea.text,task.done],
+                        function(err, rows){
+                            if(err){
+                                callback(new Error("Error de acceso a la base de datos"),null)
+                            } else {
+                                let ret = crearQuery(tarea,rows.insertId);
+
+                                let taskTag = ret.taskTag;
+                                let query = ret.query;
+                                
+                                if (taskTag.length == 0) {
+                                    connection.release();
+                                    callback(null);
+                                }else{
+                                    connection.query(
+                                        query,
+                                        taskTag,
+                                        function(err, rows){
+                                            connection.release();
+                                            if(err){
+                                                callback(new Error("Error de acceso a la base de datos"),null)
+                                            } else {
+                                                callback(null)
+                                            }
+                                        }
+                                    )
+                                }
+                                
+                            }
+                        }
+                    )
+                } else {
+                    callback(new Error("Tarea vacia"),null)
+                }
+            }
+        })
+    }
+
+
+    markTaskDone(idTask, callback) {
+        this.pool.getConnection(function(err, connection){
+            if(err){
+                callback(new Error("Error de conexion a la base de datos"), null);
+            }else{
+                connection.query(
+                    "update task set done = 1 where id = ?",
+                    [idTask],
+                    function(err, rows){
+                        connection.release();
+                        if(err){
+                            callback(new Error("Error de acceso a la base de datos"),null)
+                        } else {
+                            callback(null);
+                        }
+                    }
+                )
+            }
+        })   
+    }
+
+    deleteCompleted(email, callback) {
+        this.pool.getConnection((err,connection)=>{
+
+            if(err) callback(new Error("Error de conexión a la base de datos"));
+            else
+            connection.query("DELETE FROM task WHERE user = ? and done = 1;",[email],(err)=>{
+                
+                connection.release();
+                
+                if(err) callback(new Error("Error de acceso a la base de datos"));
+                else
+                    callback(null);
+            })
+        })
+    }
+}
+
+function crearQuery(task,id){
+    
+    let query = "insert into tag (taskId, tag) values "
+    let primera = true;
+    let taskTag = [];
+    task.tags.forEach(function(e){
+        taskTag.push(id, e);            
+        if (primera == false){
+            query += ",";
+        }
+        query += "(?,?)"
+        primera = false;
+        })
+
+    return {query:query,taskTag:taskTag};
+}
+
+function parsearTarea(task){
+    let tarea = {
+		text: "",
+		tags: []
+    };
+    
+    let expresionTags = /@(\w*)/g;
+
+    let texto = task.text;
+
+	let tags = texto.match(expresionTags);
+	
+	tarea.text = texto.replace(expresionTags, "").trim().replace(/\s+/g, " ");
+
+    if (tags != null){
+        tags.map( t => {
+            if (!tarea.tags.find(e => {
+                return "@" + e == t
+            })){
+                tarea.tags.push(t.replace(/@/, ""))
+            }
+        });
+    }
+    
+    return tarea;
+}
+
+module.exports = DAOTasks;
+   
---
+++ Practicas/Guiada 05/app.js
@@ -0,0 +1,61 @@
+"using strict";
+
+const express = require("express");
+const mysql = require("mysql");
+const config = require("./config");
+const DAO = require("./DAOTasks");
+const path = require("path");
+const body_parser = require("body-parser");
+
+const app = express();
+const pool = mysql.createPool(config.mysqlConfig);
+const taskDao = new DAO(pool);
+
+app.set("view engine","ejs");
+app.set("views", path.join(__dirname, "public", "views"));
+
+app.use(express.static("public"));
+app.use(body_parser.urlencoded({extended:false}));
+
+app.get("/tasks",(request,response)=>{
+
+    taskDao.getAllTasks("usuario@ucm.es",(err,list)=>{
+            if(err){
+                console.log(err);
+                list = [];
+            }
+            response.render("tasks.ejs",{error:err,lista:list});
+    })
+});
+
+app.post("/addTask",(request,response)=>{
+    
+    taskDao.insertTask("usuario@ucm.es",{text:request.body.tarea,done:0},(err)=>{
+        if (err){console.log(err)}
+        response.redirect("/tasks");
+    });
+});
+
+app.get("/finish/:taskId",(request,response)=>{
+    taskDao.markTaskDone(request.params.taskId,(err)=>{
+        if (err){console.log(err)}
+        response.redirect("/tasks");
+    })
+});
+
+app.get("/deleteCompleted",(request,response)=>{
+    taskDao.deleteCompleted("usuario@ucm.es",(err)=>{
+        if (err){console.log(err)}
+        response.redirect("/tasks");
+    })
+});
+
+
+app.listen(config.port,(err)=>{
+    if (err) {
+        console.log("ERROR al iniciar el servidor");
+    }
+    else {
+        console.log(`Servidor arrancado en el puerto ${config.port}`);
+    } 
+})
