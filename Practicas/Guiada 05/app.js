b6fa8bfd7cb7b44fe93236168d3b5a198f246590
carlosCharlie <32466601+carlosCharlie@users.noreply.github.com>
2018-11-22 17:02:35 +0100

    middleware control de acceso

--- Practicas/Guiada 05/app.js
+++ Practicas/Guiada 05/app.js
@@ -1,25 +1,56 @@
 "using strict";
 
+const DAOTasks = require("./DAOTasks");
+const DAOUsers = require("./DAOUsers");
+
 const express = require("express");
 const mysql = require("mysql");
 const config = require("./config");
-const DAO = require("./DAOTasks");
 const path = require("path");
 const body_parser = require("body-parser");
+const expressSession = require("express-session");
+const expressMySqlSession = require("express-mysql-session");
+
+const MySqlStore = expressMySqlSession(expressSession);
+const sessionStore = new MySqlStore(config.mysqlConfig);
 
 const app = express();
 const pool = mysql.createPool(config.mysqlConfig);
-const taskDao = new DAO(pool);
+
+const taskDao = new DAOUsers(pool);
+const userDao = new DAOUsers(pool);
 
 app.set("view engine","ejs");
 app.set("views", path.join(__dirname, "public", "views"));
 
 app.use(express.static("public"));
 app.use(body_parser.urlencoded({extended:false}));
+app.use(expressSession({resave:false, saveUninitialized:false, secret:"foobar34", store:sessionStore}));
+
+app.get("/login",(request,response)=>{
+    response.render("login.ejs",{errorMsg:null});
+});
+
+app.post("/login",(request,response)=>{
+    userDao.isUserCorrect(request.body.email,request.body.password,(err,isCorrect)=>{
+        if(err)
+            response.render("login.ejs",{errorMsg:"Error del servidor"});
+        else if(isCorrect){
+            request.session.currentUser = request.body.email;
+            response.redirect("/tasks");
+        }else
+            response.render("login.ejs",{errorMsg:"Direccion de correo y/o contraseña no válidos"});
+    })
+})
+
+app.post("logout",(request,response)=>{
+    request.session.destroy();
+    response.redirect("/login");
+})
 
 app.get("/tasks",(request,response)=>{
 
-    taskDao.getAllTasks("usuario@ucm.es",(err,list)=>{
+    taskDao.getAllTasks(request.session.currentUser,(err,list)=>{
             if(err){
                 console.log(err);
                 list = [];
@@ -30,7 +61,7 @@ app.get("/tasks",(request,response)=>{
 
 app.post("/addTask",(request,response)=>{
     
-    taskDao.insertTask("usuario@ucm.es",{text:request.body.tarea,done:0},(err)=>{
+    taskDao.insertTask(request.session.currentUser,{text:request.body.tarea,done:0},(err)=>{
         if (err){console.log(err)}
         response.redirect("/tasks");
     });
@@ -44,7 +75,7 @@ app.get("/finish/:taskId",(request,response)=>{
 });
 
 app.get("/deleteCompleted",(request,response)=>{
-    taskDao.deleteCompleted("usuario@ucm.es",(err)=>{
+    taskDao.deleteCompleted(request.session.currentUser,(err)=>{
         if (err){console.log(err)}
         response.redirect("/tasks");
     })
