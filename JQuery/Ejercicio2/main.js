81eaba2f7ea0f88a24461090c53c5298d7ea5ba0
carlosCharlie <32466601+carlosCharlie@users.noreply.github.com>
2018-12-09 19:19:13 +0100

    practica 2 apartado c

--- JQuery/Ejercicio2/main.js
+++ JQuery/Ejercicio2/main.js
@@ -7,10 +7,16 @@ function embedForm(selector,formulario,callback){
     };
     let submit = $("<input type='submit' value='Enviar'></input>");
     submit.on("click",function(){
+        
         let ret={};
         let lista = form.children();
+        
         for(let i = 0; i < lista.length-1; i = i+2){
+        
             ret[lista[i].innerText]=lista[i+1].value;
+        
+            if(!formulario[i/2].validator(lista[i+1].value))
+                alert("Error en el campo "+ lista[i].innerText);
         }
         callback(ret);
     });
@@ -21,18 +27,34 @@ function embedForm(selector,formulario,callback){
 $(function(){
     let miFormulario = [
         {
-        label: "Nombre",
-        name: "nombre"
-        },
-        {
-        label: "Apellidos",
-        name: "apellidos"
-        },
-        {
         label: "Edad",
-        name: "edad"
+        property: "edad",
+        validator: function(val) {
+        // Devuelve true si el valor pasado
+        // como parámetro es un número
+        return !isNaN(Number(val));
         }
-        ]
+        },
+        {
+            label: "Edad2",
+            property: "edad2",
+            validator: function(val) {
+            // Devuelve true si el valor pasado
+            // como parámetro es un número
+            return !isNaN(Number(val));
+            }
+            },
+            {
+                label: "Edad3",
+                property: "edad3",
+                validator: function(val) {
+                // Devuelve true si el valor pasado
+                // como parámetro es un número
+                return !isNaN(Number(val));
+                }
+                }
+        ];
+        
 
     embedForm($("body"),miFormulario,(resultado)=>console.log(resultado));
 })
